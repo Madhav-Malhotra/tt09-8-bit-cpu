f97ce462a4a707f0a7c664764ffcc5285a67e3a4
Madhva Malhotra <madhav.malhotra003@gmail.com>
2024-11-01 19:43:21 -0400

    renamed bus transceiver

---
+++ src/registers/regA_top.v
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 WAT.ai Chip Team
+ * SPDX-License-Identifier: Apache-2.0
+ * Implements register A
+ */
+
+`include "../common/bus_transceiver.v"
+
+module regA_top (
+    input wire clk,
+    input wire [7:0] bus,
+    input wire clr,
+    input wire ai_n,
+    input wire ao_n,
+    output wire [7:0] A
+);
+
+    
+
+endmodule
