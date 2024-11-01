bd5c5ce1eee1d86856b8414dea4ca0b7a7c24817
jhill0408 <jaidonhill2004@gmail.com>
2024-11-01 19:45:17 -0400

    Comments to eeprom

--- src/control/eeprom.v
+++ src/control/eeprom.v
@@ -5,27 +5,27 @@
  */
 
 module eeprom (
-    input wire ce_n,      
-    input wire oe_n,      
-    input wire we_n,     
-    input wire [10:0] addr, 
-    inout wire [7:0] data   
+    input wire ce_n,   // Active low chip enable
+    input wire oe_n,   // Active low output enable
+    input wire we_n,   // Active low write enable  
+    input wire [10:0] addr, // Address pins
+    inout wire [7:0] data   // Data input/output
 );
 
-    reg [7:0] mem_array [2047:0]; 
-    reg [7:0] data_out;
+    reg [7:0] mem_array [2047:0]; // Memory cells (should probably look into making this smaller if design does not fit on tiny tapeout)
+    reg [7:0] data_out; // Data output
 
-    assign data = (ce_n == 1'b0 && oe_n == 1'b0 && we_n == 1'b1) ? data_out : 8'bz;
+    assign data = (ce_n == 1'b0 && oe_n == 1'b0 && we_n == 1'b1) ? data_out : 8'bz; // If chip enable and output enable are active, and write enable is inactive, the data pins get output data driven to them, otherwise do not drive data wires
 
     always @(*) begin
       if (ce_n == 1'b0 && we_n == 1'b0 && oe_n == 1'b1) begin
-            mem_array[addr] = data;
+          mem_array[addr] = data; // if chip enable and write enable are active, and output enable is enactive, write the data on the data pins to specified memory addr
         end
     end
 
     always @(*) begin
       if (ce_n == 1'b0 && oe_n == 1'b0 && we_n == 1'b1) begin
-            data_out = mem_array[addr];
+          data_out = mem_array[addr]; // if chip enable and output enable are active, and write enable is not active, then update data_out register with new data
         end
     end
 
