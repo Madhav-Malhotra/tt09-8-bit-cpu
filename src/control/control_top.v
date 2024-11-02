ddd8171270b6700ded76afef94c89b8d6af53786
jhill0408 <jaidonhill2004@gmail.com>
2024-11-02 15:03:38 -0400

    control_top comments

--- src/control/control_top.v
+++ src/control/control_top.v
@@ -5,17 +5,22 @@
  */
 
 module control_top (
-  output wire hlt, mi_bar, ri, ro_bar, io_bar, ii_bar, ai_bar, ao_bar, eo_bar, su, bi_bar, oi, ce, co_bar, j_bar, fi_bar,
-  input wire cf, zf, 
-  input wire clr,
-  output wire clr_bar, 
-  input wire ir_4, ir_5, ir_6, ir_7, clk_bar
+  output wire hlt, mi_bar, ri, ro_bar, io_bar, ii_bar, ai_bar, ao_bar, eo_bar, su, bi_bar, oi, ce, co_bar, j_bar, fi_bar, // output control signals
+  input wire cf, // 9th address bit for eeproms
+  input wire zf, // 10th address bit for eeproms
+  input wire clr, // async active high clr (not actually functional in this module, should move to top level)
+  output wire clr_bar, // async active low clear (not actually functional in this module, should move to top level)
+  input wire ir_4, ir_5, ir_6, ir_7, // addr[3:6] for the eeproms
+  input wire clk_bar // inverted clock
   
 );
   
-  wire mi, ro, io, ii, ai, ao, eo, bi, co, j, fi, a0, a1, a2, not_used;
-  wire [7:0] y;
-  
+  wire mi, ro, io, ii, ai, ao, eo, bi, co, j, fi; // active high versions of active low outputs
+  wire a0, a1, a2; // addr[0:2] for eeprom, connected to output of counter
+  wire not_used; // Connects to unconnected outputs/inputs 
+  wire [7:0] y; // decoder output
+
+  // assigning active low outputs
   assign mi_bar = ~mi;
   assign ro_bar = ~ro;
   assign io_bar = ~io;
@@ -27,7 +32,8 @@ module control_top (
   assign co_bar = ~co;
   assign j_bar = ~j;
   assign fi_bar = ~fi;
-  
+
+  // instantiating eeprom with read/chip enable always active and write enable always inactive
   eeprom u_mem1 (
     .we_n(1'b1),
     .oe_n(1'b0),
@@ -36,7 +42,8 @@ module control_top (
     .data({hlt, mi, ri, ro, io, ii, ai, ao})
     
   );
-  
+
+    // instantiating eeprom with read/chip enable always active and write enable always inactive
   eeprom u_mem2 (
     .we_n(1'b1),
     .oe_n(1'b0),
@@ -44,7 +51,8 @@ module control_top (
     .addr({1'b0,zf,cf,1'b1,ir_7,ir_6,ir_5,ir_4,a2,a1,a0}),
     .data({eo, su, bi, oi, ce, co, j, fi})
   );
-  
+
+  // instantiating counter with count enable always active, and load enable always inactive
   counter_4b u_bc (
     .p_en(1'b1),
     .t_en(1'b1),
@@ -54,7 +62,8 @@ module control_top (
     .data_out({not_used, a2,a1,a0})
     
   );
-  
+
+  // instantiating always active decoder
   decoder_3b u_138 (
     .a(a0),
     .b(a1),
