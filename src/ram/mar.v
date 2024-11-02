13871ea6f498c3374d6150d4d9b087246281241a
AlexSzaboDeFi <99566718+alexanderszabodefi@users.noreply.github.com>
2024-11-02 19:11:20 -0400

    cleaning up mar connections

--- src/ram/mar.v
+++ src/ram/mar.v
@@ -1,6 +1,7 @@
 module mar (
- 	input wire [3:0] d_in,    
-    input wire [1:0] select,   
+ 	  input wire [3:0] d_in,    
+    input wire [3:0] pc_in,    // 4-bit program counter input
+    input wire select,         // Single bit select signal
     input wire clk,            
     input wire clr,            
     input wire g,              // Strobe for the multiplexer (active low)
@@ -11,21 +12,23 @@ module mar (
     wire [3:0] mux_out;  
 
     quad_2_1_mux mux (
-        .s(select[0]),   // Single select bit
-      	.g(g),           // Strobe (active low)
-      	.a(d_in),        // First input to the MUX
-        .b(4'b0000),     // Second input to the MUX (can add other inputs if necessary)
-        .y(mux_out)      // MUX output
+        .select(select),   // Single select bit
+      	.strobe(g),           // Strobe (active low)
+      	.A(d_in),        // First input to the MUX
+        .B(pc_in),     // Second input to the MUX
+        .Y(mux_out)      // MUX output
     );
 
     // Instantiate the 74LS173 register
     d_register_4b register (
-    	.clk(clk),       // Clock signal
-      	.clr(clr),       // Clear signal
-      	.g1(g1),         // Enable signal 1 (active low)
-      	.g2(g2),         // Enable signal 2 (active low)
-        .d(mux_out),     // Data input from the MUX
-        .q(mar_out)      // Register output connected to MAR_out
+    	  .CLK(clk),       // Clock signal
+      	.CLR(clr),       // Clear signal
+      	.G1_n(g1),       // Enable signal 1 (active low)
+      	.G2_n(g2),       // Enable signal 2 (active low)
+        .M(1'b0),        // Output enable control
+        .N(1'b0),        // Output enable control
+        .D(mux_out),     // Data input from the MUX
+        .Q(mar_out)      // Register output connected to MAR_out
     );
 
 endmodule
